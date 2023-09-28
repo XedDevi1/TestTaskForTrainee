2de33773a9c5397388a6b56b70ff321957cb1796
XedDevi1 <vadim.presman@gmail.com>
2023-09-28 17:22:49 +0300

    added project

---
+++ TestTask/Services/UserService.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TestTask.Data;
+using TestTask.Models;
+using TestTask.Services.Interfaces;
+using TestTask.Enums;
+
+namespace TestTask.Services
+{
+    public class UserService : IUserService
+    {
+        private readonly ApplicationDbContext _context;
+
+        public UserService(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<User> GetUser()
+        {
+            return await _context.Users
+                .OrderByDescending(u => u.Orders.Count)
+                .FirstOrDefaultAsync();
+        }
+
+        public async Task<List<User>> GetUsers()
+        {
+            return await _context.Users
+                .Where(u => u.Status == UserStatus.Inactive)
+                .ToListAsync();
+        }
+    }
+}
